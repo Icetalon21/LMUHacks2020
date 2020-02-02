9b570288b4ee73a295f2a6bb8092e0816efaf1b3
hannahlane00 <54684353+hannahlane00@users.noreply.github.com>
2020-02-01 18:34:10 -0800

    Add files via upload

--- facereg.py
+++ facereg.py
@@ -1,22 +1,33 @@
 import face_recognition
-image_me = face_recognition.load_image_file('me.jpg')
-image_me2 = face_recognition.load_image_file('me2.jpg')
-image_sean = face_recognition.load_image_file('notme.jpg')
-image_chrispratt = face_recognition.load_image_file('chrispratt.jpeg')
-image_katyperry = face_recognition.load_image_file('katyperry.jpg')
 
-me_encoding = face_recognition.face_encodings(image_me)[0]
-me2_encoding = face_recognition.face_encodings(image_me2)[0]
-sean_encoding = face_recognition.face_encodings(image_sean)[0]
-chrispratt_encoding = face_recognition.face_encodings(image_chrispratt)[0]
-katyperry_encoding = face_recognition.face_encodings(image_katyperry)[0]
+def face_recognition1(photo1, photo2, photo3, photo4, photo5, photo6):
+    image_test = face_recognition.load_image_file(photo1)
+    image_two = face_recognition.load_image_file(photo2)
+    image_three = face_recognition.load_image_file(photo3)
+    image_four = face_recognition.load_image_file(photo4)
+    image_five = face_recognition.load_image_file(photo5)
+    image_six = face_recognition.load_image_file(photo6)
 
-known_faces = [sean_encoding, chrispratt_encoding, me2_encoding,
-            katyperry_encoding]
+    test_encoding = face_recognition.face_encodings(image_test)[0]
+    two_encoding = face_recognition.face_encodings(image_two)[0]
+    three_encoding = face_recognition.face_encodings(image_three)[0]
+    four_encoding = face_recognition.face_encodings(image_four)[0]
+    five_encoding = face_recognition.face_encodings(image_five)[0]
+    six_encoding = face_recognition.face_encodings(image_six)[0]
 
-results = face_recognition.compare_faces(known_faces, me_encoding)
-num = 0
-for i in results:
-    num = num + 1
-    if i == True:
-        print("Photo", num, "is a photo of Hannah!")
+    known_faces = [two_encoding, three_encoding, four_encoding,
+                    five_encoding, six_encoding]
+    results = face_recognition.compare_faces(known_faces, test_encoding)
+    num = 0
+    for i in results:
+        num = num + 1
+        if i == True:
+            print("Photo", num, "is a photo of Hannah!")
+
+one = input("Enter test photo: ")
+two = input("Enter photo one: ")
+three = input("Enter photo two: ")
+four = input("Enter photo three: ")
+five = input("Enter photo four: ")
+six = input("Enter photo five: ")
+face_recognition1(one, two, three, four, five, six)
