e568baa4e11c0e6cbc59b6590e95712c7273d5de
sdholmes12 <54599881+sdholmes12@users.noreply.github.com>
2020-02-01 20:18:47 -0800

    Add files via upload
    website commit

---
+++ facefinder/FaceFinder.js
@@ -0,0 +1,7 @@
+const imageElement = document.querySelector("#foundimage")
+const submitButton = document.querySelector("#submitButton")
+function find(){
+  imageElement.src = "me.jpeg"
+
+}
+document.addEventListener("click", find)
